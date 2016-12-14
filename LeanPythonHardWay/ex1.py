9968ac1eafa35ff43f867907a47eef251a6a5230
davidwfranco <davidwfranco@gmail.com>
2016-12-14 09:01:04 -0200

    Exercises 1 and 3
    Exercises 1 and 3, more to take notes, than to reaaally do the exercises

---
+++ LeanPythonHardWay/ex1.py
@@ -0,0 +1,7 @@
+print "print a lot of stuff 0"
+print 'print a lot of stuff 1'
+print "print a lot of stuff 2"
+print "print a lot of stuff 3"
+print 'print a lot of """"stuff""" 4'
+print "print a lot of stuff '5'"
+print 'print a lot of stuff 6'
