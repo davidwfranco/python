f18d3a4f17073641e0c3e476b255ce265c9ff1bf
David Franco <davidwfranco@gmail.com>
2017-10-13 16:17:42 -0300

    Update and Iterate on the old execSql.py
    Build up on the old script putting up:
    - Validation to the instance past to the script
    - The option to run the script on one or more instance at time
    - Username and password parameter or "/ as sysdba" neither has been passed
    - A help section

--- NotTutorialRelated/execSql.py
+++ NotTutorialRelated/execSql.py
@@ -1,8 +1,10 @@
-import os,re
+import os
+# import time
 from subprocess import Popen, PIPE
 from optparse import OptionParser
 
-### ------------ Function Definitions ------------ ###
+# ------------ Function Definitions ------------ #
+
 
 def getAllOracleSids():
     oracleSids = []
@@ -10,33 +12,99 @@ def getAllOracleSids():
     p2 = Popen(["grep", "pmon"], stdin=p1.stdout, stdout=PIPE)
     p3 = Popen(["grep", "-v", "grep"], stdin=p2.stdout, stdout=PIPE)
     p2.stdout.close()
-    p1.stdout.close() 
+    p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
     output = p3.communicate()[0].split(os.linesep)
     for line in output:
         if line:
             oracleSids.append(line.split("_")[-1])
     return oracleSids
 
-def checkParameters(instances, sqlfile):
+
+def checkSid(instances):
     for instance in instances:
         if instance not in getAllOracleSids():
-            print "Instance \"%s\" not a valid oracle sid" % (instance)
-            break
-    if not os.path.isfile(sqlfile):
-        print "File \"%s\" not found." % (os.path.abspath(options.sqlScript))
+            parser.error("Instance \"%s\" not a valid oracle sid" % (instance))
+    return True
 
-### ------------ Main Routine ------------ ###
+
+def checkScript(sqlFile):
+    if not os.path.isfile(sqlFile):
+        parser.error("File \"%s\" not found." % (os.path.abspath(sqlFile)))
+    return True
+
+
+def runSqlQuery(sqlCommand, connectString, instanceName):
+    session = Popen(['sqlplus', '-S', connectString],
+                    stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    session.stdin.write(sqlCommand)
+    return session.communicate()
+
+
+# ------------ Main Routine ------------ #
+
 
 instanceNames = []
 parser = OptionParser()
 
-parser.add_option("-d", "--instance", dest="instanceNames", action="append", type="string",
-    help="INSTANCE NAME to connect, accept multiple entries [-d <instance1>, ... -d <instance2>]", 
-    metavar="INSTANCE_NAME")
-parser.add_option("-s", "--script", dest="sqlScript", 
-    help="SQL SCRIPT to be executed", metavar="SQL_SCRIPT")
+
+parser.add_option("-u", "--user", dest="dbUser",
+                  help="database user", metavar="DB_USER")
+parser.add_option("-p", "--password", dest="dbPassword",
+                  help="database password", metavar="DB_PASSWORD")
+parser.add_option("-i", "--instance", dest="instanceNames", action="append",
+                  type="string", help="INSTANCE NAME to connect accept " +
+                  "multiple entries [-i <instance1>, ... -i <instanceN>] or " +
+                  "[-i ALL] for all instances",
+                  metavar="INSTANCE_NAME")
+parser.add_option("-s", "--script", dest="sqlScript",
+                  help="SQL SCRIPT to be executed", metavar="SQL_SCRIPT")
 
 (options, args) = parser.parse_args()
 
-checkParameters(options.instanceNames, options.sqlScript)
+if not options.instanceNames or not options.sqlScript:
+    parser.error("Incorrect Number of Arguments")
+elif "ALL" in str(options.instanceNames).upper():
+    checkScript(options.sqlScript)
+    print "Instances: All"
+    print "Script: \"%s\"" % (str(os.path.abspath(options.sqlScript)))
+else:
+    checkScript(options.sqlScript)
+    checkSid(options.instanceNames)
 
+if options.dbUser and options.dbPassword:
+    connectString = "%s/%s" % (options.dbUser, options.dbPassword)
+else:
+    connectString = "/ as sysdba"
+
+sqlCommand = "@" + options.sqlScript
+
+logFile = "log_" + options.sqlScript.replace(".sql", "")
+
+if os.path.isfile(logFile):
+    os.remove(logFile)
+
+with open(logFile, 'a') as logF:
+    if "ALL" in options.instanceNames:
+        for sid in getAllOracleSids():
+            os.environ['ORACLE_SID'] = sid
+            queryResult, queryError = runSqlQuery(sqlCommand, connectString,
+                                                  options.instanceNames[0])
+            logF.write("Result Set %s: \n%s\n" % (sid,
+                                                  # time.strftime("%Y%m%d-%H%M"),
+                                                  queryResult))
+            if queryError:
+                logF.write("Eror %s: \n%s\n" % (sid,
+                                                # time.strftime("%Y%m%d-%H%M"),
+                                                queryError))
+    else:
+        for sid in options.instanceNames:
+            os.environ['ORACLE_SID'] = sid
+            queryResult, queryError = runSqlQuery(sqlCommand, connectString,
+                                                  options.instanceNames[0])
+            logF.write("Result Set %s: \n%s\n" % (sid,
+                                                  # time.strftime("%Y%m%d-%H%M"),
+                                                  queryResult))
+            if queryError:
+                logF.write("Eror %s: \n%s\n" % (sid,
+                                                # time.strftime("%Y%m%d-%H%M"),
+                                                queryError))
